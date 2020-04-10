7e8e3357880ea39f8b5a90dc7582989ca890fdc4
github username <github email address>
2020-04-10 09:06:21 -0700

    Adding features and style to the SettingsTable component.

--- src/component/SettingsTable.js
+++ src/component/SettingsTable.js
@@ -1,10 +1,107 @@
 import React from "react";
 import "./settingsTable.css";
-import SettingsTableBanner from "./SettingsTableBanner";
+// import SettingsTableBanner from "./SettingsTableBanner";
 const SettingsTable = (props) => {
   return (
-    <div>
-      <SettingsTableBanner />
+    <div className="tables-div">
+      <table className="camera-table">
+        <thread>
+          <tr className="camera-banner">
+            <th className="camera-title">
+              CCU <p>1688</p>
+            </th>
+            <th className="specialty-title">
+              Specialty<p>Lap 1</p>
+            </th>
+          </tr>
+        </thread>
+        <tbody>
+          <tr>
+            <td>Shutter Mode</td>
+            <td>Auto</td>
+          </tr>
+          <tr>
+            <td>Shutter Level</td>
+            <td>30</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Speed</td>
+            <td>9</td>
+          </tr>
+          <tr>
+            <td>Photometry Mode</td>
+            <td>Photometry</td>
+          </tr>
+          <tr>
+            <td>Photometry Peak/Avg</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>S Gamma</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>BG Gamma</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>MPED</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>BG Point</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>R Knee Slope</td>
+            <td>8</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table className="display-table">
+        <thread>
+          <tr>
+            <th>Display</th>
+            <th>Specialty</th>
+          </tr>
+        </thread>
+        <tbody>
+          <tr>
+            <td>Red</td>
+            <td>-35</td>
+          </tr>
+          8
+          <tr>
+            <td>Green</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Blue</td>
+            <td>7</td>
+          </tr>
+          <tr>
+            <td>Gamma</td>
+            <td>1.5</td>
+          </tr>
+          <tr>
+            <td>Brightness</td>
+            <td>45</td>
+          </tr>
+          <tr>
+            <td>Contrast</td>
+            <td>50</td>
+          </tr>
+          <tr>
+            <td>Sharpness</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
