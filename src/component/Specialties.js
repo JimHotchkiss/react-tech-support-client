20722224aa3bcc573ddf23d153595f1bc6d4a27f
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-04-24 08:28:05 -0700

    Added styling and positioning to specialty btn.

--- src/component/Specialties.js
+++ src/component/Specialties.js
@@ -1,25 +1,26 @@
 import React from "react";
+import "./specialties.css";
 
 const Specialties = (props) => {
+  console.log(props);
   const specialtiesList = props.specialties.map((specialty, index) => {
     return (
-      <a href="#" key={index}>
+      <a href='#' key={index}>
         <div
           index={index}
           className={"specialty " + (specialty.clicked ? "clicked" : "")}
-          onClick={props.handleSpecialty.bind(this, index)}
-        >
-          <p className="specialty-text">{specialty.name}</p>
+          onClick={props.handleSpecialty.bind(this, index)}>
+          <p className='specialty-text'>{specialty.name}</p>
         </div>
       </a>
     );
   });
   return (
-    <div className="specialty-component-div">
-      <div className="specialty-title-div">
-        <p className="specialty-title-tag">Specialty:</p>
+    <div className='specialty-component-div'>
+      <div className='specialty-title-div'>
+        <p className='specialty-title-tag'>Specialty:</p>
       </div>
-      <div className="specialty-list-div">{specialtiesList}</div>
+      <div className='specialty-list-div'>{specialtiesList}</div>
     </div>
   );
 };
